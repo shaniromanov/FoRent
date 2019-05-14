d70f4be4cd61f24671c3ddf0632b194821ec5406
Elad Shechter <eladvetzofia@gmail.com>
2019-05-14 17:38:04 +0300

    Adding classes Renter & ApartmentAmenities

---
+++ FoRent/Models/Renter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FoRent.Models
+{
+    public class Renter
+    {
+        [Display(Name = "תעודת זהות")]
+        public int Id { get; set; }                        
+
+        [Display(Name = "שם פרטי")]
+        public string FirstName { get; set; }             
+
+        [Display(Name = "שם משפחה")]
+        public string LastName { get; set; }               
+
+        [Display(Name = "מספר טלפון")]
+        public int Phone { get; set; }                     
+
+        [Display(Name = "כתובת מייל")]
+        public string Mail { get; set; }                    
+
+        [Display(Name = "הערות מיוחדות לגבי הדירה")]
+        public string Comment { get; set; }                 
+    }
+}
