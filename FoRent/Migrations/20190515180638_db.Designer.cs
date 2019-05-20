8129c33ebb6be2f7f00517d2ca0c58629cc0ec81
margalit s <margalite95@gmail.com>
2019-05-20 13:10:44 +0300

    Added properties in view

--- FoRent/Migrations/20190515180638_db.Designer.cs
+++ FoRent/Migrations/20190515180638_db.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FoRent.Migrations
 {
     [DbContext(typeof(FoRentContext))]
-    partial class FoRentContextModelSnapshot : ModelSnapshot
+    [Migration("20190515180638_db")]
+    partial class db
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FoRent/Migrations/20190515180638_db.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FoRent.Migrations
+{
+    public partial class db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HotWater",
+                table: "ApartmentAmenities",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Plata",
+                table: "ApartmentAmenities",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HotWater",
+                table: "ApartmentAmenities");
+
+            migrationBuilder.DropColumn(
+                name: "Plata",
+                table: "ApartmentAmenities");
+        }
+    }
+}
