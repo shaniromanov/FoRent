ea4790992ba9c779e06e0cfe21d0da4e0ecd1bb7
Norma <nechamay94@gmail.com>
2019-05-22 01:01:14 +0300

    Availability set

--- FoRent/Models/Availability.cs
+++ FoRent/Models/Availability.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace FoRent.Models
 {
     public class Availability
     {
+        [Display(Name = "צ'אק אין")]
+        public DateTime CheckIn { get; set; }
+
+        [Display(Name = "צ'אק אאוט")]
+        public DateTime CheckOut { get; set; }
     }
 }
