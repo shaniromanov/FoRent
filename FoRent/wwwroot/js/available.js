aa00b9b66d7da7afcfc56b329fdbbb62bd2e2800
shani <shaniromanov@gmail.com>
2019-09-04 17:14:49 +0300

    try again

---
+++ FoRent/wwwroot/js/available.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $("#btnSave").click(function () {
+        //alert("");  
+        var form = $(this).parent("form");
+        $.ajax({
+            type: "POST",
+            url: form.attr("asp-action"),
+            data: form.serialize,
+            dataType: "application/json; charset=utf-8",
+            contentType: date,
+            success: function (response) {
+                if (response.success)
+                    alert(response.responseText);
+
+                //swal("התאריך נקלט במערכת", "באפשרותך להזין תאריכים נוספים", "success");
+            },
+
+        });
+        return false;
+    });
+});
